8c4dc911ac2eda73621b94eaf6e13e21dd1c711d
abhishek <abhiskg20@gmail.com>
2022-10-10 21:02:05 +0600

    router setup complete from diffrent pages

---
+++ src/utils/router.jsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "../components/ErrorPage";
+import Root from "../layout/Root";
+import Home from "../screens/Home";
+import Statistics from "../screens/Statistics";
+import Blog from "../screens/Blog";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "home",
+        element: <Home />,
+      },
+      {
+        path: "statistics",
+        element: <Statistics />,
+      },
+      {
+        path: "blog",
+        element: <Blog />,
+      },
+    ],
+  },
+]);
+
+export default router;
