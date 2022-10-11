43f5ae767163f6b83a36a46c26089aef73c6a7f9
abhishek <abhiskg20@gmail.com>
2022-10-11 20:31:50 +0600

    Error page design complete

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,5 +1,23 @@
+import { useLottie } from "lottie-react";
+import { useRouteError } from "react-router-dom";
+import error from "../assets/error.json";
+
 const ErrorPage = () => {
-  return <div>Error</div>;
+  const options = {
+    animationData: error,
+    loop: true,
+  };
+  const { View } = useLottie(options);
+  const routeError = useRouteError();
+  console.log(routeError);
+  return (
+    <div className="grid place-items-center">
+      {View}
+      <div className="text-red-500">
+        {routeError.statusText || routeError.message}
+      </div>
+    </div>
+  );
 };
 
 export default ErrorPage;
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom";
+import { ArrowSmallRightIcon } from "@heroicons/react/24/solid";
 
 const Quiz = ({ quiz }) => {
   const { id, name, logo, total } = quiz;
   return (
-    <div className="border p-3 rounded-lg">
-      <div className="bg-slate-500 rounded-xl ">
+    <div className="rounded-lg border p-3">
+      <div className="rounded-xl bg-slate-500 ">
         <img src={logo} alt="" className="mx-auto w-full" />
       </div>
-      <p className="mt-2 font-semibold text-lg">Topic: {name}</p>
-      <p>Total: {total}</p>
-      <Link to={`/quiz/${id}`}>Start Quiz</Link>
+      <p className="mt-2 text-lg font-semibold">Topic: {name}</p>
+      <p>Total Quiz: {total}</p>
+      <div className="flex items-center">
+        <Link to={`/quiz/${id}`}>Start Practice</Link>
+        <ArrowSmallRightIcon className="h-5 w-5" />
+      </div>
     </div>
   );
 };
