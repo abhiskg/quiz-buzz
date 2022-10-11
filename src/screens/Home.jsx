b7affc6dad11018019345b3b2c70c8a2c2fcce46
abhishek <abhiskg20@gmail.com>
2022-10-11 20:11:07 +0600

    Header image added

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,12 +1,27 @@
 import { useLoaderData } from "react-router-dom";
+import { useLottie } from "lottie-react";
 import Quiz from "../components/Quiz";
+import main from "../assets/main.json";
 
 const Home = () => {
   const { data } = useLoaderData();
-
+  const options = {
+    animationData: main,
+    loop: true,
+  };
+  const { View } = useLottie(options);
   return (
-    <div className="">
-      <div className="custom-width mx-auto grid md:grid-cols-3 grid-cols-2 gap-5 mt-5">
+    <div className="custom-width mx-auto">
+      <div className="flex items-center justify-between ">
+        <div>
+          <h1 className="text-4xl font-bold">Quiz Buzz</h1>
+          <p className="text-lg font-medium">
+            A fun and effective way to test your skills.{" "}
+          </p>
+        </div>
+        <div>{View}</div>
+      </div>
+      <div className=" mt-5 grid grid-cols-2 gap-5 md:grid-cols-3">
         {data.map((quiz) => (
           <Quiz key={quiz.id} quiz={quiz} />
         ))}
